c3a16bddefdc092f36af416fc505b865fbe7f910
吴问应 <wuwenying@theduapp.com>
2019-09-18 11:21:46 +0800

    1

---
+++ test1/src/application/test1.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/18
+ * Time: 11:09
+ */
+
+namespace Test1;
+
+class Cache {
+
+    public function add(){
+        echo time();
+    }
+
+    public function info(){
+        phpinfo();
+    }
+
+}
