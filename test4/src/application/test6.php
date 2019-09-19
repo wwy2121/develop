5645d7affdc92cdc051558760c87b0850a95b79e
吴问应 <wuwenying@theduapp.com>
2019-09-19 11:33:54 +0800

    1

---
+++ test4/src/application/test6.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/19
+ * Time: 11:33
+ */
+
+namespace Test6;
+
+class Test6 {
+
+    public function del(){
+        echo "echo 6".time();
+    }
+
+}
