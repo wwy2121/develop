450d919c33a17e34134eae927067c63c767720ec
吴问应 <wuwenying@theduapp.com>
2019-09-19 11:20:57 +0800

    1

---
+++ test5/src/application/test5.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/18
+ * Time: 11:09
+ */
+
+namespace Test5;
+
+class Test5 {
+
+    public function add(){
+        echo time();
+    }
+
+    public function info(){
+        phpinfo();
+    }
+
+}
