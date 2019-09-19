c4ff0bebd9f31d89408e187fcf67308127b6a0ad
吴问应 <wuwenying@theduapp.com>
2019-09-19 11:27:00 +0800

    1

--- test3/src/application/test4.php
+++ test3/src/application/test4.php
@@ -10,8 +10,22 @@ namespace Test4;
 
 class Test4 {
 
-    public function test() {
-        echo "test4 info";
+    public function add(){
+        echo "echo 4".time();
+    }
+
+    public function info(){
+        phpinfo();
     }
 
 }
+
+//namespace Test4;
+//
+//class Test4 {
+//
+//    public function test() {
+//        echo "test4 info";
+//    }
+//
+//}
