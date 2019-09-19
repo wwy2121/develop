594b9f0f2f1430143df2496e2b8179913917549e
吴问应 <wuwenying@theduapp.com>
2019-09-19 11:45:27 +0800

    1

---
+++ test7/src/test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wwy2121
+ * Date: 2019/9/19
+ * Time: 11:44
+ */
+
+namespace Test7;
+
+class Test {
+    public function add() {
+        echo "test7";
+    }
+}
